6d765cd2c4959f92177a464bed0ee4d7d05aea55
ansiiis1@gmail.com <geox9838@gmail.com>
2017-11-15 12:04:08 +0200

    20171115_12_00

---
+++ sin_caur_summu_ver1_C02.py
@@ -0,0 +1,19 @@
+#_*_ coding: utf-8 _*_
+from math import sin
+
+x = inpuit ("Lietotāj, lūdzu, ievadi argumentu (x):")
+y = sin(x)
+print "sin(%.2f) = %.2f%(x,y)"
+
+a0 = (-1)**0*x**1/(1)
+S0 = a0
+print "a0 = %.2f S0 = %.2f"%(a0,S0)
+
+a1 = (-1)**0*x**3/(1*2*3)
+S1 = a0 + a1
+print "a1 = %.2f S1 = %.2f"%(a1,S1)
+
+a2 = (-1)**0*x**3/(1*2*3)
+S1 = a0 + a1
+print "a2 = %.2f S2 = %.2f"%(a2,S2)
+
